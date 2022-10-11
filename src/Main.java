001e96160ffe68d3ab35c91b99a3a544609b254f
Felix Chan <felixc63@nycstudents.net>
2022-10-11 12:23:18 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main (String[] args){
+        Student student = new Student("Abby", "Smith", 2023);
+        student.addTestScore(95);
+        student.addTestScore(98);
+        System.out.println(student.getStudentInfo());
+    }
 }
